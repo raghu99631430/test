3d01a10d147d6be41936635cc5640e59f73f9a5b
raghvendra <varshaenterprises207@gmail.com>
2025-04-15 11:43:33 +0530

    this is testing

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " Hi raghavendra where are you"
+echo " are you going to office "
+echo " could able to handle international depatures D2 counter "
+echo " kindly hand over cash and currency "  
+
